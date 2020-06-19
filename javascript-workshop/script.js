11179ffe14484e0b662aeea4c57e7d079c893e5b
Narapun <ome_001@hotmail.com>
2020-06-19 10:48:29 +0700

    js

---
+++ javascript-workshop/script.js
@@ -0,0 +1,19 @@
+var message = "mmmmmm";
+console.log(message);
+
+const message1 = "const const"; //ต้องใส่ค่า และใส่ค่าอื่นอีกไม่ได้ ใช้ let แทน
+//let message1 = "const const"; //ต้องใส่ค่า และใส่ค่าอื่นอีกไม่ได้ ใช้ let แทน
+//message1 = "const constๅ"; 
+console.log(message1);
+
+alert("Hello Javascript");
+console.log("Hello Javascrip");
+var a = 10;
+var b = 20;
+var c = a + b;
+console.log("a + b = ",c);
+
+let human;
+let name = "Narapun";
+human = name;
+console.log("human =",human);
